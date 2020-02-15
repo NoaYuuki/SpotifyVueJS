967c77c503717dc2d6dc0779477d71e92f42c9d4
NoaYuuki <50140159+NoaYuuki@users.noreply.github.com>
2020-02-15 18:30:58 +0100

    Add files via upload

---
+++ views/Favorites.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app" class="central-box">
+      <h1>Welcome to your favorite songs !</h1><br>
+      <p>Here you will find all your favorite songs. Simply add a title to this list by marking it with a star!</p>
+    <router-view/>
+  </div>
+</template>
+
+<style>
+body{
+  background-color: #191414;
+}
+
+h1{
+    font-weight: 600;
+}
+
+.central-box{
+    margin: 0 200px; 
+    color: grey;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #ffffff;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #ffffff;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ views/Search.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="central-box">
+    <input class="search" type="text" placeholder="Rechercher ...">
+  </div>
+</template>
+
+<style>
+body{
+  background-color: #191414;
+  overflow: hidden;
+}
+
+.search{
+    background-color: #ffffff;
+    padding: 10px;
+    margin: 0 300px;
+    border-radius: 18px;
+    border: none;
+}
+
+::placeholder {
+  color: rgb(139, 139, 139);
+}
+
+.central-box{
+    margin: 0 100px;
+}
+
+</style>
